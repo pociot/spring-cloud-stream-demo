d793687e88cf14d6491670396f1d2c74e408101c
kamilpociot <kamil.pociot@me.com>
2019-01-22 16:07:17 +0100

    Dynamic bindings working.

--- src/main/java/com/pociot/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
+++ src/main/java/com/pociot/springcloudstreamdemo/SpringCloudStreamDemoApplication.java
@@ -1,11 +1,15 @@
 package com.pociot.springcloudstreamdemo;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.binder.HeaderMode;
 import org.springframework.cloud.stream.binder.kafka.properties.KafkaProducerProperties;
 import org.springframework.cloud.stream.binding.BinderAwareChannelResolver.NewDestinationBindingCallback;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +26,11 @@ public class SpringCloudStreamDemoApplication {
 	@Bean
 	public NewDestinationBindingCallback<KafkaProducerProperties> dynamicConfigurer() {
 		return (name, channel, props, extended) -> {
+			props.setHeaderMode(HeaderMode.raw);
+			Map<String, String> extendedProps = new HashMap<>();
+			extendedProps.put("key.serializer", StringSerializer.class.getName());
+			extendedProps.put("value.serializer", StringSerializer.class.getName());
+			extended.setConfiguration(extendedProps);
 			log.info("Dynamic configurer - name:{}, channel:{}, props:{}", name, channel, props);
 			extended.setConfiguration(Collections.singletonMap("client.id", name + UUID.randomUUID().toString()));
 		};
